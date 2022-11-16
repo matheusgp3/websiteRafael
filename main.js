ac6d67fefa5c11f10438bd88af1f98da27dc03d8
matheus <matheusgp3@hotmail.com>
2022-11-16 18:41:52 -0300

    upando primeiro prototipo website rafael

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+function adjustheight(height){
+    top = height * 0.05
+    middle =  height * 0.65
+    bottom = height * 0.3
+    document.querySelector('header').style.height = top + "px"
+    document.querySelector('main').style.height = middle + "px"
+    document.querySelector('footer').style.height = bottom + "px"
+}
+
+
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+
+adjustheight(height)
+*/
